05d39869b429e1cb7e82a28ba954ffe08750ad0e
Blotify <satvik040306@gmail.com>
2025-10-22 20:00:00 +0530

    Add neonate bonus feature (Spec 16) to print most recent PID

---
+++ src/neonate.h
@@ -0,0 +1,16 @@
+#ifndef NEONATE_H
+#define NEONATE_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <termios.h>
+#include <ctype.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <signal.h>
+#include <sys/select.h>
+void pnc(const char*c);int ixkp();pid_t gmrp();long long gpst(pid_t p);
+#endif
