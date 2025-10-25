1a1ccf31f428a32f1bff8a7aed60c22a57a573fc
Blotify <satvik040306@gmail.com>
2025-10-25 18:30:00 +0530

    Add iMan command (Spec 17) to fetch man pages via sockets

---
+++ src/iman.h
@@ -0,0 +1,16 @@
+#ifndef IMAN_H
+#define IMAN_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <ctype.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#define BUFFER_SIZE 4096
+#define HOST "man.he.net"
+#define PORT "80"
+void gcw(const char*i,char*c,size_t sz);void sht(char*i,char*o);void ue(const char*s,char*d,size_t sz);void sr(int s,const char*c);void rapr(int s,const char*c);void pic(const char*i);
+#endif
